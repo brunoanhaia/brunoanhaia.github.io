db25211a9b9cd820f7efb52e47e5122aeaff5146
brunoanhaia <bruno.anhaia@fit-tecnologia.org.br>
2019-10-09 14:51:25 -0300

    Added Resume

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 const headers = new Headers();
 const init = { method: 'GET', headers: headers, mode: 'cors', cache: 'default' }
+var hiro = {
+  name: "Bruno Anhaia",
+  age: "26",
+  education: [
+    { institution: "Sorocaba Technological College (FATEC)", status: "in progress", name: "System Analysis and Development - Information Technology" },
+    { institution: "University of Campinas (UNICAMP)", status: "in progress", name: "Masters in Electrical Engineering"},
+    { institution: "Sorocaba College of Engineering (FACENS)", status: "finished", name: "Bachelor in Electrical Engineering"},
+    { institution: "TOkyo University of Electrical Communications (UEC)", status: "finished", name: "Sandwich degree"},
+  ],
+  work: [
+    { company: "FIT - Institute of Technology", period: "2019/01 ~ NOW", experience: "C/C++, Python, Batch" },
+    { company: "VA Engenharia", period: "2018/03 ~ 2019/01", experience: "PHP, MySQL, jQuery, JavaScript, HTML, SASS, Python Flask" },
+  ]
+
+}
 
 window.onload = () => {
-  const section_gh_projects__ul = document.querySelector(".section__gh--projects ul");
-  const div_img_profile__figure__figcaption = document.querySelector(".div__image--profile figure figcaption");
-  const div_img_profile__figure__company = document.querySelector(".div__image--profile figure .company");
-  const div_img_profile__figure__image_profile = document.querySelector(".div__image--profile figure .image--profile");
+  const gh_projects__ul = document.querySelector(".section__gh--projects ul");
 
+  const main_img_profile__figure__figcaption = document.querySelector(".div__image--profile figure figcaption");
+  const main_img_profile__figure__company = document.querySelector(".div__image--profile figure .company");
+  const main_img_profile__figure__image_profile = document.querySelector(".div__image--profile figure .image--profile");
+
+  const resume__wrapper__pre = document.querySelector(".section__resume .wrapper pre");
+  
   const open = document.querySelector('.open-menu');
   const close = document.querySelector('.close-menu');
   const nav = document.querySelector('.navbar');
@@ -37,9 +55,9 @@ window.onload = () => {
   fetch('https://api.github.com/users/brunoanhaia')
     .then(response => response.json())
     .then(data => {
-      div_img_profile__figure__figcaption.innerHTML += `${data.name}`;
-      div_img_profile__figure__company.innerHTML += `${data.company}`;
-      div_img_profile__figure__image_profile.setAttribute("src",`${data.avatar_url}`);
+      main_img_profile__figure__figcaption.innerHTML += `${data.name}`;
+      main_img_profile__figure__company.innerHTML += `${data.company}`;
+      main_img_profile__figure__image_profile.setAttribute("src",`${data.avatar_url}`);
     })
     .catch(error => console.error(error))
     
@@ -47,8 +65,17 @@ window.onload = () => {
     .then(response => response.json())
     .then(data => {
       data.forEach((e) => {
-        section_gh_projects__ul.innerHTML += `<li><a href='${e.html_url}'>${e.name}</li>`;
+        gh_projects__ul.innerHTML += `<li><a href='${e.html_url}'>${e.name}</li>`;
       });
     })
     .catch(error => console.error(error))
+
+    resume__wrapper__pre.innerHTML = JSON.stringify(hiro, null, '  ');
+
+/*     Object.keys(hiro).forEach((key)=> {
+      hiro[key].forEach((element)=> {
+
+        console.log(key + "=>" + element)
+      })
+    }); */
 };
