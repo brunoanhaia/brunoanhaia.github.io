f4b1f56a0ed152ce39107df2c383c48b08d9e0ab
brunoanhaia <bruno.anhaia@fit-tecnologia.org.br>
2019-10-09 16:06:57 -0300

    Design fix

--- main.js
+++ main.js
@@ -1,24 +1,47 @@
 const headers = new Headers();
 const init = { method: 'GET', headers: headers, mode: 'cors', cache: 'default' }
-var hiro = {
+const personal_data = {
   name: "Bruno Anhaia",
   age: "26",
   education: [
-    { institution: "Sorocaba Technological College (FATEC)", status: "in progress", name: "System Analysis and Development - Information Technology" },
-    { institution: "University of Campinas (UNICAMP)", status: "in progress", name: "Masters in Electrical Engineering"},
-    { institution: "Sorocaba College of Engineering (FACENS)", status: "finished", name: "Bachelor in Electrical Engineering"},
-    { institution: "TOkyo University of Electrical Communications (UEC)", status: "finished", name: "Sandwich degree"},
+      {
+          institution: "Sorocaba Technological College (FATEC)",
+          status: "in progress",
+          name: "System Analysis and Development - Information Technology"
+      },
+      {
+          institution: "University of Campinas (UNICAMP)",
+          status: "in progress",
+          name: "Masters in Electrical Engineering"
+      },
+      {
+          institution: "Sorocaba College of Engineering (FACENS)",
+          status: "finished",
+          name: "Bachelor in Electrical Engineering"
+      },
+      {
+          institution: "TOkyo University of Electrical Communications (UEC)",
+          status: "finished",
+          name: "Sandwich degree"
+      },
   ],
   work: [
-    { company: "FIT - Institute of Technology", period: "2019/01 ~ NOW", experience: "C/C++, Python, Batch" },
-    { company: "VA Engenharia", period: "2018/03 ~ 2019/01", experience: "PHP, MySQL, jQuery, JavaScript, HTML, SASS, Python Flask" },
+      {
+          company: "FIT - Institute of Technology",
+          period: "2019/01 ~ NOW",
+          experience: "C/C++, Python, Batch"
+      },
+      {
+          company: "VA Engenharia",
+          period: "2018/03 ~ 2019/01",
+          experience: "PHP, MySQL, jQuery, JavaScript, HTML, SASS, Python Flask"
+      }
   ]
-
 }
 
 window.onload = () => {
   const gh_projects__ul = document.querySelector(".section__gh--projects ul");
-
+  
   const main_img_profile__figure__figcaption = document.querySelector(".div__image--profile figure figcaption");
   const main_img_profile__figure__company = document.querySelector(".div__image--profile figure .company");
   const main_img_profile__figure__image_profile = document.querySelector(".div__image--profile figure .image--profile");
@@ -70,12 +93,6 @@ window.onload = () => {
     })
     .catch(error => console.error(error))
 
-    resume__wrapper__pre.innerHTML = JSON.stringify(hiro, null, '  ');
+    resume__wrapper__pre.innerHTML = JSON.stringify(personal_data, null, '  ');
 
-/*     Object.keys(hiro).forEach((key)=> {
-      hiro[key].forEach((element)=> {
-
-        console.log(key + "=>" + element)
-      })
-    }); */
 };
